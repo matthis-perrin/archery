383f81c341dfcb5abca3e0b4d8c6b3dc43e2925e
Matthis Perrin <matthis.perrin@gmail.com>
2022-01-09 16:15:10 +0100

    Publish app on testflight

---
+++ ios/dummy.swift
@@ -0,0 +1,8 @@
+//
+//  dummy.swift
+//  arrowpoints
+//
+//  Created by Matthis Perrin on 09/01/2022.
+//
+
+import Foundation
--- src/stores.tsx
+++ src/stores.tsx
@@ -1,17 +1,5 @@
-import {createDataStore} from './data_store';
 import {createMapStore} from './map_data_store';
 import {Session, SessionId} from './models';
-
-interface AppState {
-  page: string;
-}
-
-const stateStore = createDataStore<AppState>('state', {page: 'test'}, console.error);
-export const getAppState = stateStore.getData;
-export const setAppState = stateStore.setData;
-export const useAppState = stateStore.useData;
-
-//
 
 const sessionsStore = createMapStore<SessionId, Session>('session', console.error);
 export const getSession = sessionsStore.getData;
@@ -21,5 +9,5 @@ export const useSessions = sessionsStore.useAllData;
 export const deleteSession = sessionsStore.deleteData;
 
 export async function awaitStoresLoaded(): Promise<void> {
-  await Promise.all([stateStore.awaitLoaded(), sessionsStore.awaitLoaded()]);
+  await Promise.all([sessionsStore.awaitLoaded()]);
 }
